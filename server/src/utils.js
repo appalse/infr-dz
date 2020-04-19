1a9af730e12459bdba95a0c2bcc78fe5c0be6ba2
appalse <frontend-hh@yandex.ru>
2020-04-19 17:36:32 +0300

    fix printError

--- server/src/utils.js
+++ server/src/utils.js
@@ -9,6 +9,10 @@ function printError(err) {
     if (err.status && err.data) {
         console.error('ERROR STATUS: ', err.status);
         console.error('ERROR DATA: ', err.data);
+    } else if (err.response && err.response.status && err.response.statusText && err.response.data) {
+        console.error('ERROR STATUS: ', err.response.status);
+        console.error('ERROR STATUS TEXT: ', err.response.statusText);
+        console.error('ERROR DATA: ', err.response.data);
     } else {
         console.error('ERROR: ', err);
     }

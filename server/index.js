e3d7cd9a08bf66c8954aa842c11865d6df765474
appalse <frontend-hh@yandex.ru>
2020-04-19 17:14:07 +0300

    write methods in index.js on server

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
-const path = require('path');
-const axios = require('axios');
-const https = require('https');
+
+
 const express = require('express');
 const cors = require('cors');
+const {notifyBuildResult} = require('./src/notifyBuildResult');
+const {notifyAgent} = require('./src/notifyAgent');
+const {processBuilds} = require('./src/processBuilds');
+const { BUILD_REQUEST_DELAY, 
+        PORT, 
+        URL_NOTIFY_AGENT, 
+        URL_NOTIFY_BUILD_REQUEST } = require('./src/constants');
 
-// build api
-const api = axios.create({
-    baseURL: 'https://hw.shri.yandex/api',
-    headers: { Authorization: "Bearer " + process.env.API_TOKEN },
-    httpsAgent: new https.Agent({ rejectUnauthorized: false })
-});
 
 // Express.js
 const app = express();
 app.use(express.json());
-app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(cors());
 
-const PORT = 3001;
+// Ходит за заданиями на сборку в базу (API) 
+// и передает задание на сборку одному из агентов. 
+// Потом принимает результаты сборки и записывает их в БД (API).
+setTimeout(processBuilds, BUILD_REQUEST_DELAY);
+
+// URLs
+//  зарегистрировать агента. 
+// В параметрах хост и порт, на котором запущен агент
+app.post(URL_NOTIFY_AGENT, notifyAgent);
+// сохранить результаты сборки. 
+// В параметрах — id сборки, статус, лог (stdout и stderr процесса).
+app.post(URL_NOTIFY_BUILD_REQUEST, notifyBuildResult);
+
+
 app.listen(PORT);
-console.log('Build-server is listening on localhost:${PORT}');
+console.log(`Build-server is listening on localhost:${PORT}`);
